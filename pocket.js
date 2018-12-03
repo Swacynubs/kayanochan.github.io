cf1ae82a651c89d02dfd10dc7d1d6cdd4d42b2e7
schoolaccount2019 <mjian1@ocdsb.ca>
2018-12-03 13:28:42 -0500

    use svg

--- pocket.js
+++ pocket.js
@@ -1,102 +1,104 @@
-function Sticker(side,pos,color) {
-    
-    this.side=side||0;
-    this.pos=pos||0;
-    this.color=color||'#000000';
-    
-    this.draw = function(c) {
-        var p;
-        p = c.getContext("2d");
-        p.fillStlye=this.color;
-        var x, y;
-        // U,R,F,D,L,B
-        switch(this.side) {
+window.onload = function () {
+    "use strict";
+    function Sticker(side, pos, color) {
+
+        this.side = side || 0;
+        this.pos = pos || 0;
+        this.color = color || '#000000';
+
+        this.draw = function (c) {
+            var p, x, y;
+            p = c.getContext("2d");
+            p.fillStlye = this.color;
+            // U,R,F,D,L,B
+            switch (this.side) {
             case 0:
-                x=101;
-                y=1;
+                x = 101;
+                y = 1;
                 break;
             case 1:
-                x=201;
-                y=101;
+                x = 201;
+                y = 101;
                 break;
             case 2:
-                x=101;
-                y=101;
+                x = 101;
+                y = 101;
                 break;
             case 3:
-                x=101;
-                y=201;
+                x = 101;
+                y = 201;
                 break;
             case 4:
-                x=1;
-                y=101;
+                x = 1;
+                y = 101;
                 break;
             default:
-                x=301;
-                y=101;
-        }
-        switch(this.pos) {
-            case 1;
-                x+=50;
+                x = 301;
+                y = 101;
+            }
+            switch (this.pos) {
+            case 1:
+                x += 50;
                 break;
             case 2:
-                x+=50;
-                y+=50;
+                x += 50;
+                y += 50;
                 break;
             case 3:
-                y+=50;
+                y += 50;
                 break;
-        }
-        p.fillRect(x,y,48,48);
-        p.rect(x,y,48,48);
-        p.stroke();
+            }
+            p.fillRect(x, y, 48, 48);
+            p.rect(x, y, 48, 48);
+            p.stroke();
+        };
     }
-}
 
-function drawSticker(c,sticker) {
-    var p;
-    p = c.getContext("2d");
-    p.fillStlye=sticker.color;
-    var x, y;
-    // U,R,F,D,L,B
-    switch(sticker.side) {
+    function drawSticker(c, sticker) {
+        var p, x, y;
+        p = c.getContext("2d");
+        p.fillStlye = sticker.color;
+        // U,R,F,D,L,B
+        switch (sticker.side) {
         case 0:
-            x=101;
-            y=1;
+            x = 101;
+            y = 1;
             break;
         case 1:
-            x=201;
-            y=101;
+            x = 201;
+            y = 101;
             break;
         case 2:
-            x=101;
-            y=101;
+            x = 101;
+            y = 101;
             break;
         case 3:
-            x=101;
-            y=201;
+            x = 101;
+            y = 201;
             break;
         case 4:
-            x=1;
-            y=101;
+            x = 1;
+            y = 101;
             break;
         default:
-            x=301;
-            y=101;
-    }
-    switch(sticker.pos) {
-        case 1;
-            x+=50;
+            x = 301;
+            y = 101;
+        }
+        switch (sticker.pos) {
+        case 1:
+            x += 50;
             break;
         case 2:
-            x+=50;
-            y+=50;
+            x += 50;
+            y += 50;
             break;
         case 3:
-            y+=50;
+            y += 50;
             break;
+        }
+        p.fillRect(x, y, 48, 48);
+        p.rect(x, y, 48, 48);
+        p.stroke();
     }
-    p.fillRect(x,y,48,48);
-    p.rect(x,y,48,48);
-    p.stroke();
-}
+    
+};
